42542f5efc5ea5db81e10662a279520bf64d7055
Oumar Wade <oumar951@outlook.fr>
2022-09-15 15:23:54 +0000

    Modules Learned

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PokemonTypeColorPipe} from "./pipes/pokemon-type-color.pipe";
+import {BorderCardDirective} from "./directives/border-card.directive";
+import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
+import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const pokemonRoutes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent },
+];
+
+
+@NgModule({
+  declarations: [
+      DetailPokemonComponent,
+      ListPokemonComponent,
+      PokemonTypeColorPipe,
+      BorderCardDirective
+  ],
+  imports: [
+     CommonModule,
+     RouterModule.forChild(pokemonRoutes)
+  ]
+})
+export class PokemonModule { }
